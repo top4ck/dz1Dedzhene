45c67e630e81f2d87b6062c26a517208c2c41ba7
MAXIMATOR <dedgenimatios@gmail.com>
2023-10-20 22:55:21 +0430

    Dz №1

---
+++ main.py
@@ -0,0 +1,18 @@
+#a=int(input("Введіть число: "))
+#if a % 2 == 0:
+    #print("Введене число парне.")
+#else:
+    #print("Введене число непарне.")
+
+
+#a=input("Введіть рядок: ")
+#aeiou = 0
+#for char in a:
+    #if char in "aeiou":
+        #aeiou += 1
+#print("Кількість голосних букв у рядку:", aeiou)
+
+
+from math import factorial
+number = int(input('Введите число: '))
+print(factorial(number))
